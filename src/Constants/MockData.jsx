aead1a709fc9dffaf2704d0d6aa3f8f19a00af23
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-09-02 19:20:10 +0530

    3 Commit

--- src/Constants/MockData.jsx
+++ src/Constants/MockData.jsx
@@ -1,14 +1,35 @@
+import image1 from "../Assests/1.png";
+import image2 from "../Assests/2.png";
+import image3 from "../Assests/3.png";
+import image4 from "../Assests/4.png";
+import image5 from "../Assests/5.png";
+import image6 from "../Assests/6.png";
+import image7 from "../Assests/7.png";
+import image8 from "../Assests/8.png";
+// import image9 from "../Assests/9.png";
+// import image10 from "../Assests/10.png";
+// import image11 from "../Assests/11.png";
+// import image12 from "../Assests/12.png";
+// import image13 from "../Assests/13.png";
+// import image14 from "../Assests/14.png";
+// import image15 from "../Assests/15.png";
+ import image16 from "../Assests/16.png";
+
 export const cardsData = [
-    { id: 1, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 2, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 3, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 4, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 5, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 6, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 7, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 8, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 9, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 10, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 11, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: false },
-    { id: 12, imageUrl: 'https://t4.ftcdn.net/jpg/06/39/80/75/360_F_639807553_uwUKbAadh2moLIRDQo4vzYJo1CeQLNYH.jpg', add: true },
+  { id: 1, imageUrl: image1, add: false },
+  { id: 2, imageUrl: image2, add: false },
+  { id: 3, imageUrl: image3, add: false },
+  { id: 4, imageUrl: image4, add: false },
+  { id: 5, imageUrl: image5, add: false },
+  { id: 6, imageUrl: image6, add: false },
+  { id: 7, imageUrl: image7, add: false },
+  { id: 8, imageUrl: image8, add: false },
+  //   { id: 9, imageUrl: image9, add: false },
+  //   { id: 10, imageUrl: image10, add: false },
+  //   { id: 11, imageUrl: image11, add: false },
+  //   { id: 12, imageUrl: image12, add: false },
+  //   { id: 13, imageUrl: image13, add: false },
+  //   { id: 14, imageUrl: image14, add: false },
+  //   { id: 15, imageUrl: image15, add: false },
+  { id: 16, imageUrl: image16, add: true },
 ];
